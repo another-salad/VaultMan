1bfbecc717faaad3b6207a8665b93d8389233cd8
anothersalad <48966874+another-salad@users.noreply.github.com>
2024-08-14 23:39:12 +0100

    vaultman please

---
+++ vaultman/vaultman.psm1
@@ -0,0 +1,58 @@
+
+Import-Module Microsoft.PowerShell.SecretManagement
+Import-Module Microsoft.PowerShell.SecretStore
+
+## Secret vault fun
+# https://learn.microsoft.com/en-us/powershell/utility-modules/secretmanagement/get-started/using-secretstore?view=ps-modules
+
+Function Set-VaultManNonInteractiveVault {
+    [CmdletBinding()]
+    param (
+        [string]$VaultName
+    )
+    # You can't set some vaults to be non-interactive and some to be interactive. It's all or nothing.
+    # If you want an interactive vault, simply run, Set-VaultManVault.
+    # Even in this non-interactive mode, you'll need to set an initial password, once the config is complete you won't be
+    # prompted for this again.
+    Write-Warning "This will set ALL VAULTS to non-interactive, no authentication mode. Think about this wisely."
+    Set-SecretStoreConfiguration -Interaction None -Authentication None -Scope CurrentUser
+    Set-VaultManVault -vaultName $VaultName
+}
+
+Function Set-VaultManVault {
+    [CmdletBinding()]
+    param (
+        [string]$VaultName
+    )
+    Register-SecretVault -Name $VaultName -ModuleName Microsoft.PowerShell.SecretStore
+    Get-SecretVault -Name $VaultName
+}
+
+Function Set-VaultManSecureStringToVault {
+    [CmdletBinding()]
+    param (
+        [string]$SecretName,
+        [string]$VaultName,
+        [securestring]$SecretValue
+    )
+    Set-Secret -Name $SecretName -Secret $SecretValue -Vault $VaultName
+}
+
+Function Set-VaultManSecureStringToVaultInteractive {
+    [CmdletBinding()]
+    param (
+        [string]$SecretName,
+        [string]$VaultName
+    )
+    $SecretValue = Read-Host -Prompt "Enter secret value" -AsSecureString
+    Set-VaultManSecureStringToVault -SecretName $SecretName -VaultName $VaultName -SecretValue $SecretValue
+}
+
+Function Get-VaultManSecureStringFromVault {
+    [CmdletBinding()]
+    param (
+        [string]$SecretName,
+        [string]$VaultName
+    )
+    Get-Secret -Name $SecretName -Vault $VaultName
+}
